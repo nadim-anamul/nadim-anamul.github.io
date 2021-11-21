8f02ab41f4bec4f2a4f678de2fdd7236673c8c01
nadim-anamul <nadim.csm@gmail.com>
2021-11-21 11:25:09 +0600

    final v-1.0.0

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-(function($) {
-    "use strict";
+(function ($) {
+    'use strict';
+    // scroll header function
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.header').addClass('header-fixed');
+        } else {
+            $('.header').removeClass('header-fixed');
+        }
+    });
 
+    // slider carousel
+    $('.slider-start').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 1,
+        autoplay: false,
+        dots: true,
+        smartSpeed: 600,
+    });
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-        $('.header').addClass("header-fixed");
-    } else {
-        $('.header').removeClass("header-fixed");
-    }
+    // featured image carousel
+    $('#featured-image').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 1,
+        autoplay: false,
+        dots: true,
+        smartSpeed: 600,
+    });
 
-
-});
-
-
-$('.slider-start').owlCarousel({
-    loop: true,
-    nav: false,
-    items: 1,
-    autoplay: false,
-    dots: true,
-    smartSpeed: 600,
-});
-$('#featured-image').owlCarousel({
-    loop: true,
-    nav: false,
-    items: 1,
-    autoplay: false,
-    dots: true,
-    smartSpeed: 600,
-});
-$('#reviewSlider').owlCarousel({
-    loop: true,
-    nav: false,
-    items: 1,
-    autoplay: false,
-    dots: true,
-    smartSpeed: 600,
-});
-
-
+    // testimonial carousel
+    $('#reviewSlider').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 1,
+        autoplay: false,
+        dots: true,
+        smartSpeed: 600,
+    });
 })(jQuery);
